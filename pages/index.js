5b76aba4a34ac20e2978bc4c72366f37ba615968
nayan <sontiya.nayan@gmail.com>
2024-12-31 22:46:31 +0530

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,114 +1,235 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import { io } from "socket.io-client";
+import { useRef, useEffect, useState } from "react";
+import { FiVideo, FiVideoOff, FiMic, FiMicOff } from "react-icons/fi";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const configuration = {
+  iceServers: [
+    {
+      urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
+    },
+  ],
+  iceCandidatePoolSize: 10,
+};
+const socket = io("http://localhost:3001", { transports: ["websocket"] });
+
+let pc;
+let localStream;
+let startButton;
+let hangupButton;
+let muteAudButton;
+let remoteVideo;
+let localVideo;
+socket.on("message", (e) => {
+  if (!localStream) {
+    console.log("not ready yet");
+    return;
+  }
+  switch (e.type) {
+    case "offer":
+      handleOffer(e);
+      break;
+    case "answer":
+      handleAnswer(e);
+      break;
+    case "candidate":
+      handleCandidate(e);
+      break;
+    case "ready":
+      // A second tab joined. This tab will initiate a call unless in a call already.
+      if (pc) {
+        console.log("already in call, ignoring");
+        return;
+      }
+      makeCall();
+      break;
+    case "bye":
+      if (pc) {
+        hangup();
+      }
+      break;
+    default:
+      console.log("unhandled", e);
+      break;
+  }
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+async function makeCall() {
+  try {
+    pc = new RTCPeerConnection(configuration);
+    pc.onicecandidate = (e) => {
+      const message = {
+        type: "candidate",
+        candidate: null,
+      };
+      if (e.candidate) {
+        message.candidate = e.candidate.candidate;
+        message.sdpMid = e.candidate.sdpMid;
+        message.sdpMLineIndex = e.candidate.sdpMLineIndex;
+      }
+      socket.emit("message", message);
+    };
+    pc.ontrack = (e) => (remoteVideo.current.srcObject = e.streams[0]);
+    localStream.getTracks().forEach((track) => pc.addTrack(track, localStream));
+    const offer = await pc.createOffer();
+    socket.emit("message", { type: "offer", sdp: offer.sdp });
+    await pc.setLocalDescription(offer);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function handleOffer(offer) {
+  if (pc) {
+    console.error("existing peerconnection");
+    return;
+  }
+  try {
+    pc = new RTCPeerConnection(configuration);
+    pc.onicecandidate = (e) => {
+      const message = {
+        type: "candidate",
+        candidate: null,
+      };
+      if (e.candidate) {
+        message.candidate = e.candidate.candidate;
+        message.sdpMid = e.candidate.sdpMid;
+        message.sdpMLineIndex = e.candidate.sdpMLineIndex;
+      }
+      socket.emit("message", message);
+    };
+    pc.ontrack = (e) => (remoteVideo.current.srcObject = e.streams[0]);
+    localStream.getTracks().forEach((track) => pc.addTrack(track, localStream));
+    await pc.setRemoteDescription(offer);
+
+    const answer = await pc.createAnswer();
+    socket.emit("message", { type: "answer", sdp: answer.sdp });
+    await pc.setLocalDescription(answer);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function handleAnswer(answer) {
+  if (!pc) {
+    console.error("no peerconnection");
+    return;
+  }
+  try {
+    await pc.setRemoteDescription(answer);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+async function handleCandidate(candidate) {
+  try {
+    if (!pc) {
+      console.error("no peerconnection");
+      return;
+    }
+    if (!candidate) {
+      await pc.addIceCandidate(null);
+    } else {
+      await pc.addIceCandidate(candidate);
+    }
+  } catch (e) {
+    console.log(e);
+  }
+}
+async function hangup() {
+  if (pc) {
+    pc.close();
+    pc = null;
+  }
+  localStream.getTracks().forEach((track) => track.stop());
+  localStream = null;
+  startButton.current.disabled = false;
+  hangupButton.current.disabled = true;
+  muteAudButton.current.disabled = true;
+}
 
 export default function Home() {
-  return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              pages/index.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  startButton = useRef(null);
+  hangupButton = useRef(null);
+  muteAudButton = useRef(null);
+  localVideo = useRef(null);
+  remoteVideo = useRef(null);
+  useEffect(() => {
+    hangupButton.current.disabled = true;
+    muteAudButton.current.disabled = true;
+  }, []);
+  const [audiostate, setAudio] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+  const startB = async () => {
+    try {
+      localStream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: { echoCancellation: true },
+      });
+      localVideo.current.srcObject = localStream;
+    } catch (err) {
+      console.log(err);
+    }
+
+    startButton.current.disabled = true;
+    hangupButton.current.disabled = false;
+    muteAudButton.current.disabled = false;
+
+    socket.emit("message", { type: "ready" });
+  };
+
+  const hangB = async () => {
+    hangup();
+    socket.emit("message", { type: "bye" });
+  };
+
+  function muteAudio() {
+    if (audiostate) {
+      localVideo.current.muted = true;
+      setAudio(false);
+    } else {
+      localVideo.current.muted = false;
+      setAudio(true);
+    }
+  }
+  return (
+    <main className="container  ">
+      <div className="video bg-main">
+        <video
+          ref={localVideo}
+          className="video-item"
+          autoPlay
+          playsInline
+          src=" "
+        ></video>
+        <video
+          ref={remoteVideo}
+          className="video-item"
+          autoPlay
+          playsInline
+          src=" "
+        ></video>
+      </div>
+
+      <div className="btn">
+        <button
+          className="btn-item btn-start"
+          ref={startButton}
+          onClick={startB}
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          <FiVideo />
+        </button>
+        <button className="btn-item btn-end" ref={hangupButton} onClick={hangB}>
+          <FiVideoOff />
+        </button>
+        <button
+          className="btn-item btn-start"
+          ref={muteAudButton}
+          onClick={muteAudio}
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          {audiostate ? <FiMic /> : <FiMicOff />}
+        </button>
+      </div>
+    </main>
   );
 }
